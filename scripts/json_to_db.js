58bc5c0a000b387bbc7fb226dd13a1f2bf7a6b1d
Ethan Trott <me@ethantrott.com>
2024-06-09 12:20:55 -0400

    more json parsing

--- scripts/json_to_db.js
+++ scripts/json_to_db.js
@@ -5,12 +5,17 @@ const sconfig = require("./script_config.json");
 const dir = sconfig.filepath;
 const filename = sconfig.test_file;
 
-function parseFile(file){
-    const birdData = require(file);
+function parseFile(filepath, filename){    
+    const birdData = require(filepath);
+
+    //filename:             1999-01-08T04:05:06.429Z.json
+    //proper (postgres):    1999-01-08 04:05:06
+    var timestamp = filename.replace(".json","").replace("T" ," ").split('.')[0];;
 
     for (bird in birdData.birds){
         console.log(birdData.birds[bird].location);
     }
 }
 
-parseFile(dir+filename);
+parseFile(dir+filename, filename);
+console.log(filename);
