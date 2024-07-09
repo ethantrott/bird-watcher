6fc867a47308b0eb6ebc2bb7e48086febfb54fae
Ethan Trott <me@ethantrott.com>
2024-07-09 16:51:26 -0400

    comment cleanup

--- main.js
+++ main.js
@@ -7,14 +7,15 @@ const PORT = 80;
 // host everything in public dir
 app.use(express.static('public'));
 
+// ----- API endpoints --------
 // formatting for json responses
 app.set('json spaces', 2);
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/api/history/latest', async(req, res) => {
     const data = await db.getLatestData();
     res.json(data);
 });
+// -----------------------------
 
 // start server
 app.listen(PORT, (error) =>{
