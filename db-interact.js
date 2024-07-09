5900f61eadbe1792a5437d1aad74c776c103ecd3
Ethan Trott <me@ethantrott.com>
2024-07-09 15:03:01 -0400

    create basic api call for latest data

---
+++ db-interact.js
@@ -0,0 +1,48 @@
+const { Client } = require('pg');
+
+const config = require("./config.json");
+
+async function connectDB(){
+    var client = new Client({
+        host: config.host,
+        port: config.port,
+        user: config.username,
+        password: config.password
+    });
+
+    // connect
+    await client.connect();
+
+    return client;
+}
+
+async function disconnectDB(client){
+    // exit
+    await client.end(); 
+}
+
+// gets latest bird data and returns it
+async function getLatestData(){
+    const client = await connectDB();
+    const res = await client.query(`SELECT * FROM history WHERE time_utc = (SELECT MAX(time_utc) FROM history);`);
+    
+    var dataToReturn = res.rows[0];
+    dataToReturn.birds = [];
+
+    for (const moment of dataToReturn.bird_moments){
+        const momentRes = await client.query(`SELECT * FROM moments WHERE moment_id = '${moment}';`);
+        const data = momentRes.rows[0];
+
+        //remove unneeded data
+        delete data['moment_id'];
+
+        dataToReturn.birds.push(data);
+    }
+
+    delete dataToReturn['bird_moments'];
+
+    disconnectDB(client);
+    return dataToReturn;
+}
+
+module.exports = { getLatestData }
--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const db = require('./db-interact');
 
 const app = express();
 const PORT = 80;
 
+// host everything in public dir
 app.use(express.static('public'));
 
+// formatting for json responses
+app.set('json spaces', 2);
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/api/history/latest', async(req, res) => {
+    const data = await db.getLatestData();
+    res.json(data);
+});
+
+// start server
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("web server running on port "+ PORT)
